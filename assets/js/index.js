7012d8c933a3745c65b3a53be4190daf6e212635
Ebenezer Arkoh-Amoah <ebenezer.arkoh-amoah@amalitech.com>
2022-09-25 22:10:59 +0000

    restructuring the code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,95 +1,97 @@
-const inputResult = () => {
-  let userInputs = document.getElementById("answer").value;
-  let previousUserInput = userInputs[userInputs.length - 1];
-  if (
-    previousUserInput == "+" ||
-    previousUserInput == "-" ||
-    previousUserInput == "*" ||
-    previousUserInput == "/"
-  ) {
-    return (document.getElementById("answer").value = "SyntaxError");
-  } else {
-    return (document.getElementById("answer").value = parseInt(eval(
-      document.getElementById("answer").value
-    )).toLocaleString("en"));
+let clearButton = document.getElementById("reset");
+let resultButton = document.getElementById("results");
+let deleteButton = document.getElementById("delete");
+let themeOne = document.getElementById("first-theme");
+let themeTwo = document.getElementById("second-theme");
+let themeThree = document.getElementById("third-theme");
+let buttons = document.querySelectorAll(".input-button")
+let screen = document.getElementById("answer") ;
+let sign = document.getElementById("left-box");
+let digits = [0,1,2,3,4,5,6,7,8,9,"."]
+let operators = ["=","x","/","+","-"]
+let operand1 = "" ;
+let operand2 = "" ;
+let operator = "" ;
+
+
+function isDigit(value){
+  return digits.includes(value)
+}
+function isOperator(value){
+  return operators.includes(value)
+}
+
+function processClick(e){
+  let value = e.target.innerText;
+  if (isOperator(value) && value == "x") {
+    operator = "*";
+    return sign.innerText = "x";
+}
+  if (isOperator(value) && value !== "x") {
+    operator = value;
+return  sign.innerText = operator;
   }
-};
-
-const resetInputs = () => {
-  return (document.getElementById("answer").value = " ");
-};
-
-
-function displayNumbers(value) {
-  let userInputs = document.getElementById("answer").value;
-  let firstUserInput = userInputs[0];
-  if (
-    firstUserInput == "/" ||
-    firstUserInput == "*" ||
-    firstUserInput == "u" ||
-    firstUserInput == "S"
-  ) {
-    return (document.getElementById("answer").value = value);
-  } else {
-    return (document.getElementById("answer").value += value);
+  else if(operator.length < 1){
+    operand1 += value;
+    return screen.value = operand1;
+  }
+  else {
+    operand2 += value;
+    return screen.value = operand2;
   }
 }
 
-function createFocus() {
-  return document.getElementById("answer").focus();
+function convertInputToLocaleString() {
+  let num = screen.value;
+  let convertedNum = Number(num.replace(",", "")).toLocaleString("en-US");
+  if (num.includes(".")) {
+    return screen.value = convertedNum.replace(".",".");
+  }
+  else {
+    return screen.value = Number(num.replace(",","")).toLocaleString("en-US")
+  }
+}
+
+function inputResult(){
+  let result = eval(operand1 + operator + operand2);
+  operand2 = ""
+  operand1 = result;
+  let num = operand1;
+  let convertedNum = Number(num).toLocaleString("en-US");
+  return  screen.value = convertedNum;
   
+};
+
+function deleteInput(){
+  if (screen.value == operand1) {
+    let lastDigit = operand1.charAt(operand1.length - 1);
+    operand1 = operand1.replace(lastDigit,""); 
+    return screen.value = operand1
+  }
+  else if (screen.value == operand2){
+     let lastDigit = operand2.charAt(operand2.length - 1);;
+    operand2 = operand2.replace(lastDigit,""); 
+    return screen.value = operand2;
+  }
+  else if(operand1.charAt() == false){
+    operand1.toString();
+     let lastDigit = operand1.charAt(operand1.length - 1);
+    operand1 = operand1.replace(lastDigit,""); 
+    return screen.value = operand1
+  }
 }
 
-const deleteInput = () => {
-  let userInputs = document.getElementById("answer").value;
-  userInputs = userInputs.split("");
-  userInputs[userInputs.length - 1] = "";
-  userInputs = userInputs.join("");
-  return (document.getElementById("answer").value = userInputs);
-};
-
-const operatorInput = (value) => {
-  let userInputs = document.getElementById("answer").value;
-  let previousUserInput = userInputs[userInputs.length - 1];
-  let firstUserInput = userInputs[0];
-  if (
-    previousUserInput == "+" ||
-    previousUserInput == "*" ||
-    previousUserInput == "/" ||
-    previousUserInput == "-"
-  ) {
-    userInputs = userInputs.split("");
-    userInputs.pop();
-    userInputs = userInputs.join("");
-    return (userInputs = document.getElementById("answer").value =
-      userInputs + value);
-  } else if (firstUserInput == "u" || firstUserInput == "S") {
-    return (document.getElementById("answer").value = value);
-  } else {
-    return (document.getElementById("answer").value = userInputs + value);
-  }
-};
-
-const fullStopInput = (value) => {
-  let userInputs = document.getElementById("answer").value;
-  let previousUserInput = userInputs[userInputs.length - 1];
-  let firstUserInput = userInputs[0];
-  if (previousUserInput == ".") {
-    userInputs = userInputs.split("");
-    userInputs.pop();
-    userInputs = btn.join("");
-    return (userInputs = document.getElementById("answer").value =
-      userInputs + value);
-  } else if (firstUserInput == "u" || firstUserInput == "S") {
-    return (document.getElementById("answer").value = value);
-  } else {
-    return (document.getElementById("answer").value = userInputs + value);
-  }
-};
+function clearOperands() {
+  operand2 = "";
+  operand1 = "";
+  operator = "";
+  sign.innerText = "";
+  return( screen.value = "");
+}
 
 let root = document.querySelector(":root");
 
-let defaultTheme = () => {
+function defaultTheme(){
   return (
     root.style.setProperty("--body-background", "#3A4663"),
     root.style.setProperty("--main-color", "#FFFFFF"),
@@ -112,7 +114,7 @@ let defaultTheme = () => {
   );
 };
 
-let secondTheme = () => {
+function secondTheme(){
   return (
     root.style.setProperty("--body-background", "#E6E6E6"),
     root.style.setProperty("--main-color", "#36362C"),
@@ -135,7 +137,7 @@ let secondTheme = () => {
   );
 };
 
-let thirdTheme = () => {
+function thirdTheme(){
   return (
     root.style.setProperty("--body-background", "#17062A"),
     root.style.setProperty("--main-color", "#FFE53D"),
@@ -157,3 +159,16 @@ let thirdTheme = () => {
     root.style.setProperty("--answer-button", "#93FFF8")
   );
 };
+
+
+themeOne.addEventListener("click", defaultTheme);
+themeTwo.addEventListener("click", secondTheme);
+themeThree.addEventListener("click", thirdTheme);
+deleteButton.addEventListener("click", deleteInput);
+resultButton.addEventListener("click", inputResult);
+clearButton.addEventListener("click", clearOperands);
+
+buttons.forEach(btn => btn.addEventListener("click", processClick))
+
+buttons.forEach( btn => btn.addEventListener("click", convertInputToLocaleString
+))
